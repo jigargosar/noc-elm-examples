256472977f0962fad1624598ebff105513a1cf12
jigargosar <jigar.gosar@gmail.com>
2024-11-07 15:07:40 +0530

    added walking anim

--- src/Example_01.elm
+++ src/Example_01.elm
@@ -1,12 +1,14 @@
 module Example_01 exposing (..)
 
 import Browser
+import Browser.Events
 import Html exposing (..)
 import Html.Attributes as HA exposing (style)
 import List.Extra
 import Random exposing (Generator)
 import Svg
 import Svg.Attributes as SA
+import Time
 
 
 main =
@@ -18,23 +20,38 @@ main =
         }
 
 
+type alias Model =
+    { ticks : Int
+    }
+
+
+init : () -> ( Model, Cmd msg )
 init () =
-    ( {}, Cmd.none )
+    ( Model 0, Cmd.none )
 
 
-subscriptions _ =
-    Sub.none
+type Msg
+    = OnTick
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.ticks >= ic.limit then
+        Sub.none
+
+    else
+        Time.every (1000 / 60) (always OnTick)
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        OnTick ->
+            ( { model | ticks = model.ticks + 1 }, Cmd.none )
 
 
-view _ =
-    viewSample
-
-
-viewSample =
+view : Model -> Html msg
+view model =
     Svg.svg
         [ viewBoxCentered (tupleRepeat 500)
         , style "width" "500"
@@ -46,45 +63,46 @@ viewSample =
         --, style "shape-rendering" "crispEdges"
         --, style "shape-rendering" "optimizeSpeed"
         ]
-        (randomWalkerPoints 100
-            |> stepWithInitialSeed 1
+        (randomWalkerPoints model.ticks
+            |> stepWithInitialSeed ic.seed
             |> List.map viewPoint
         )
 
 
 type alias Config =
-    { radius : Float, scale : Float, opacity : Float }
+    { seed : Int
+    , limit : Int
+    , radius : Float
+    , scale : Float
+    , opacity : Float
+    }
 
 
 ic : Config
 ic =
-    { radius = 5, scale = 5 * 1.5, opacity = 0.3 }
-
-
-
---pointRadius =
---    5
---
---
---pointScale =
---    pointRadius * 1.5
---
---
---pointOpacity =
---    0.3
+    { seed = 16
+    , limit = 2000
+    , radius = 5
+    , scale = 5 * 1.5
+    , opacity = 0.2
+    }
 
 
 randomWalkerPoints : Int -> Generator (List Point)
 randomWalkerPoints ct =
     Random.list ct randomDirVec
-        |> Random.map (offsetsToPoints)
+        |> Random.map offsetsToPoints
 
 
 offsetsToPoints : List Point -> List Point
 offsetsToPoints offsets =
     offsets
-        |> List.Extra.scanl add2 ( 0, 0 )
+        |> List.Extra.scanr add2 originPt
         |> List.map (vecScale ic.scale)
+
+
+originPt =
+    tupleRepeat 0
 
 
 randomDirVec : Generator Point
@@ -99,47 +117,6 @@ randomDirVec =
         --, ( 1, 1 )
         --, ( -1, -1 )
         ]
-
-
-
---points =
---    let
---        randomPoint : Generator Point
---        randomPoint =
---            -- Random.constant ( 100, 0 )
---            let
---                gen =
---                    -- Random.float -250 250
---                    Random.float -200 200
---            in
---            Random.pair gen gen
---    in
---    -- [ ( 100, 0 ) ]
---    Random.step (Random.list 1000 randomPoint) (Random.initialSeed 1)
---        |> Tuple.first
---
---
---
---viewPoint2 ( x, y ) =
---    Svg.rect
---        [ SA.width "2"
---        , SA.height "2"
---
---        --, SA.x "1"
---        --, SA.y "1"
---        --, SA.x "-5"
---        --, SA.y "-5"
---        --, style "fill" "#FFF"
---        , style "stroke-linecap" "round"
---        , style "stroke-linejoin" "round"
---        , style "fill" "none"
---        , style "stroke" "#FFF"
---        , translate ( x, y )
---        , style "stroke-width" "1"
---
---        --, style "opacity" "0.5"
---        ]
---        []
 
 
 viewPoint ( x, y ) =
