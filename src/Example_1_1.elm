c8bb8ad9b64fba25849e6860fa7dbc1b593bce9d
jigargosar <jigar.gosar@gmail.com>
2024-11-16 15:36:16 +0530

    rendered static ball

--- src/Example_1_1.elm
+++ src/Example_1_1.elm
@@ -1,7 +1,33 @@
 module Example_1_1 exposing (main)
 
 import Html
+import Html.Attributes exposing (style)
+import Svg
+import Svg.Attributes as SA
 
 
 main =
-    Html.text "1_1"
+    view ()
+
+
+view _ =
+    Svg.svg
+        [ SA.viewBox "-320 -120 640 240"
+        , style "display" "block"
+        , style "width" "640"
+        , style "fill" "none"
+        , style "stroke" "none"
+        , style "background" "#000"
+        , style "shape-rendering" "geometric-precision"
+
+        --, style "shape-rendering" "optimizeSpeed"
+        --, style "shape-rendering" "crispEdges"
+        ]
+        [ Svg.circle
+            [ SA.r "20"
+            , style "fill" "#444"
+            , style "stroke" "#fff"
+            , SA.strokeWidth "6"
+            ]
+            []
+        ]
